f8e52308e1ba8989d3adc1098a9e0a5b0151b093
rizkijadida <rizkijadida@gmail.com>
2024-11-19 12:50:46 +0700

    masuk

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -6,8 +6,7 @@ const Homepage = () => {
       <p className="font-bold mx-20 md:text-9xl mt-28">Hi, I'm Rizki</p>
       <br />
       <p className="flex font-bold text-center md:mx-96 md:text-4xl">
-        -FullStack Web Developer. I like to learn aboout Fullstack Web
-        Development. Wecome to my Portofolio
+        -FullStack Web Developer. Wecome to my Portofolio
       </p>
     </div>
   );
