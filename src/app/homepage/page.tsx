99857c9c360d9bd41eed4158588aed268d5570c2
rizkijadida <rizkijadida@gmail.com>
2024-11-19 12:49:58 +0700

    feat: commit

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -6,7 +6,7 @@ const Homepage = () => {
       <p className="font-bold mx-20 md:text-9xl mt-28">Hi, I'm Rizki</p>
       <br />
       <p className="flex font-bold text-center md:mx-96 md:text-4xl">
-        -Student of Purwadhika. I like to learn aboout Fullstack Web
+        -FullStack Web Developer. I like to learn aboout Fullstack Web
         Development. Wecome to my Portofolio
       </p>
     </div>
